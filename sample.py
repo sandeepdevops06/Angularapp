488b02abb1644ad7cf82f1326aa8e8d54bf8f83a
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-10 18:22:20 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -12,8 +12,12 @@ print("x is 1.")
 
 print("Goodbye, World!")
 
+class MyClass(object):
+    def __init__(self):
+        self.message = 'Hello'
+		
 target = -5
 num = 3
 
-target =- num  # Noncompliant; target = -3. Is that really what's meant?
-target =+ num # Noncompliant; target = 3
+target = -num  # Compliant; intent to assign inverse value of num is clear
+target += num
